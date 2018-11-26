b9aa9cbcba5a79f95af225c0728bbedcab9cdb61
willie <chi8857@gmail.com>
2018-11-26 11:26:41 +0800

    make it better

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ import time
 app = Flask(__name__)
 
 # 填入你的 message api 資訊
-line_bot_api = LineBotApi('YxTSr5T6yB1vNRmZNXGo5tqBMwgelhxFvjHdG9Ymi+fHREqFOtKpVeNEtYrX2I4uEd0z5rCpJuL3ei8hCXF9xCWzrg3Bw7oPzf4D+QzqJKazGEAIs9u7N50v1odKWUlNWtGCa9twFhm0rshrm6bqTAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3c698809cb17ebbe581aebe712c76baf')
+line_bot_api = LineBotApi('gi2NdIngaDaOHkfhfBWR8zLH2OAgaTWE2+2lE/cO9qe99Bu0ssQdn7cwb6fkyv/+oHMWYgLueyuymUi6O6uDVzF0RUt4Hzyem3b6BrqaPoPC1KPue96tK5cuOFa/egyOIB69ZWrdJ2cDxwQFon2h1wdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('d3a982e72a276216a43939540e247f34')
 
 # 設定你接收訊息的網址，如 https://YOURAPP.herokuapp.com/callback
 @app.route("/callback", methods=['POST'])
@@ -61,7 +61,7 @@ def handle_message(event):
 	if msg=="5":
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text="磁簧警報"))
 	else:
-		content = "{}: isom love {}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), event.message.text)
+		content = "{}: isom love :{}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), event.message.text)
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
 
 	
